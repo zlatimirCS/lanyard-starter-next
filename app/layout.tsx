29a4148933ebdf66fdbad5fdd5f79af2252d2841
ZlatimirRamac-CS <zlatimir.ramac@concordsoft.solutions>
2026-01-14 14:20:43 +0100

    added header into public layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["400", "600", "700"],
   variable: "--font-inter",
 });
 
--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,17 @@
 function Header() {
   return (
-    <header className="bg-white shadow-sm border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <h1 className="text-2xl font-bold text-test">Lanyard Builder</h1>
-          </div>
-          <nav className="flex items-center space-x-4">
-            <a href="#" className="text-gray-600 hover:text-gray-900">
-              Home
-            </a>
-            <a href="#" className="text-gray-600 hover:text-gray-900">
-              About
-            </a>
-          </nav>
-        </div>
+    <header className="bg-white">
+      <div className="max-w-[1440px] min-h-[142px] mx-auto py-[25px] px-4 flex items-center justify-between">
+        {/*Logo*/}
+        <h1 className="text-black tracking-[-0.96px] text-[48px] font-bold leading-[120%]">
+          Lanyard Builder
+        </h1>
+        {/*Logo*/}
+        {/*Menu*/}
+        <h3 className="text-gray-900 text-[24px] font-semibold leading-[120%] tracking-[-0.48px]">
+          Choose product
+        </h3>
+        {/*Menu*/}
       </div>
     </header>
   );
